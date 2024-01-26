c45607857afd75af4249933d852ed304056bcdd7
Fran Arant <francis.arant@gmail.com>
2024-01-26 17:20:32 -0500

    Add desktop-view

--- components.js
+++ components.js
@@ -1,2 +1,3 @@
 import {} from './components/random-nanoid.js'
 import {} from './components/dithered-image.js'
+import {} from './components/desktop-view.js'
---
+++ components/desktop-view.js
@@ -0,0 +1,32 @@
+
+const desktop_view_css = new CSSStyleSheet();
+desktop_view_css.replaceSync(`
+.desktop-view {
+	position: absolute;
+	top: 0;
+	left: 0;
+	bottom: 0;
+	right: 0;
+	box-sizing: border-box;
+	border: 5px solid black;
+}
+`)
+
+const desktop_view_html = document.createElement("template")
+desktop_view_html.innerHTML = `
+<div class="desktop-view">
+	<slot></slot>
+</div>
+`
+
+class DesktopView extends HTMLElement {
+	connectedCallback() {
+		this.attachShadow({ mode: "open" })
+		this.shadowRoot.appendChild(
+			desktop_view_html.content.cloneNode(true)
+		)
+		this.shadowRoot.adpotStyleSheets([desktop_view_css])
+	}
+}
+
+window.customElements.define('desktop-view', DesktopView)
