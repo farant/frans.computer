6a9f224f267d841cc92fe319f475a57fbd77e2f9
Fran Arant <francis.arant@gmail.com>
2024-01-29 19:24:16 -0500

    so weird

--- components/markdown-view.js
+++ components/markdown-view.js
@@ -53,11 +53,16 @@ class MarkdownView extends HTMLPreElement {
     }
 
     let input = dedent(this.innerHTML.trim());
+    console.log("input", input);
+    console.log("decoded", html_decode(input));
+
     let text = marked(html_decode(input));
 
     console.log("text", text);
+    /*
     console.log("input", input);
     console.log("decoded", html_decode(input));
+    */
 
     this.innerHTML = text;
     this.style.display = "block";
