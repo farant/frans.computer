35267690ed46f8daa8fa83e9513d9762f4068ec2
Fran Arant <francis.arant@gmail.com>
2024-01-29 19:06:12 -0500

    try html decode

--- components/markdown-view.js
+++ components/markdown-view.js
@@ -9,6 +9,11 @@ marked.use({
   },
 });
 
+function html_decode(input) {
+  var doc = new DOMParser().parseFromString(input, "text/html");
+  return doc.documentElement.textContent;
+}
+
 class MarkdownView extends HTMLPreElement {
   constructor() {
     super();
@@ -34,7 +39,7 @@ class MarkdownView extends HTMLPreElement {
       this.innerHTML = this.children[0].innerHTML;
     }
 
-    this.innerHTML = marked(dedent(this.innerHTML.trim()));
+    this.innerHTML = html_decode(marked(dedent(this.innerHTML.trim())));
     this.style.display = "block";
   }
 }
