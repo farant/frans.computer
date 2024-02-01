f6b77e8160d44aee01c36f7f5cf93e7f637da164
Fran Arant <francis.arant@gmail.com>
2024-02-01 17:11:07 -0500

    Update PugEditor to fetch content from target-href

--- components/pug-editor.js
+++ components/pug-editor.js
@@ -8,12 +8,18 @@ class PugEditor extends HTMLElement {
       this._initial_inner_html = document.querySelector(
         this.getAttribute("target")
       ).innerHTML;
-    } else if (this.innerHTML) {
+    } else if (this.innerHTML && this.getAttribute("target-href")) {
       this._initial_inner_html = this.innerHTML;
     }
   }
 
   async connectedCallback() {
+    if (this.getAttribute("target-href")) {
+      this._initial_inner_html = await fetch(
+        this.getAttribute("target-href")
+      ).then((response) => response.text());
+    }
+
     let pug = await HtmlToPug(this._initial_inner_html);
 
     this.innerHTML = `<monaco-editor language="pug">${pug}</monaco-editor>`;
