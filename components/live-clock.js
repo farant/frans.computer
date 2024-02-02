9f35b15167bdf4d3a582b3200b9613ad345aa1e3
Fran Arant <francis.arant@gmail.com>
2024-02-02 16:12:24 -0500

    Add styling to clock output

--- components/live-clock.js
+++ components/live-clock.js
@@ -1,5 +1,9 @@
 const live_clock_css = new CSSStyleSheet();
 live_clock_css.replaceSync(`
+.clock-output {
+    font-family: monospace;
+    color: blue;
+}
 `);
 
 const live_clock_html = document.createElement("template");
@@ -9,39 +13,39 @@ live_clock_html.innerHTML = `
 `;
 
 class LiveClock extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({ mode: "open" });
-    this.shadowRoot.adoptedStyleSheets = [live_clock_css];
-    this.shadowRoot.appendChild(live_clock_html.content.cloneNode(true));
-  }
+	constructor() {
+		super();
+		this.attachShadow({ mode: "open" });
+		this.shadowRoot.adoptedStyleSheets = [live_clock_css];
+		this.shadowRoot.appendChild(live_clock_html.content.cloneNode(true));
+	}
 
-  connectedCallback() {
-    if (this.interval) {
-      clearInterval(this.interval);
-    }
-    this.update_clock();
-    this.interval = setInterval(() => {
-      this.update_clock();
-    }, 1000);
-  }
+	connectedCallback() {
+		if (this.interval) {
+			clearInterval(this.interval);
+		}
+		this.update_clock();
+		this.interval = setInterval(() => {
+			this.update_clock();
+		}, 1000);
+	}
 
-  update_clock() {
-    let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    let ampm = hours >= 12 ? "PM" : "AM";
+	update_clock() {
+		let date = new Date();
+		let hours = date.getHours();
+		let minutes = date.getMinutes();
+		let seconds = date.getSeconds();
+		let ampm = hours >= 12 ? "PM" : "AM";
 
-    hours = hours % 12;
-    hours = hours ? hours : 12;
-    minutes = minutes < 10 ? `0${minutes}` : minutes;
-    seconds = seconds < 10 ? `0${seconds}` : seconds;
+		hours = hours % 12;
+		hours = hours ? hours : 12;
+		minutes = minutes < 10 ? `0${minutes}` : minutes;
+		seconds = seconds < 10 ? `0${seconds}` : seconds;
 
-    this.shadowRoot.querySelector(
-      ".clock-output"
-    ).textContent = `${hours}:${minutes}:${seconds} ${ampm}`;
-  }
+		this.shadowRoot.querySelector(
+			".clock-output"
+		).textContent = `${hours}:${minutes}:${seconds} ${ampm}`;
+	}
 }
 
 window.customElements.define("live-clock", LiveClock);
